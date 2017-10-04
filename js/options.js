362f487460fe1a0f625618550454f969784dc175
Travis Ali <ali.gilgil1@gmail.com>
2017-10-04 19:22:45 -0400

    Send event to GA when user clicks full recipe

--- js/options.js
+++ js/options.js
@@ -6,4 +6,15 @@ _gaq.push(['_trackPageview']);
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = 'https://ssl.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+    $('#link').click(handleOutboundLinkClicks);
 })();
+
+function handleOutboundLinkClicks(event) {
+    ga('send', 'event', {
+      eventCategory: 'Outbound Link',
+      eventAction: 'click',
+      eventLabel: event.target.href,
+      transport: 'beacon'
+    });
+}
